a27abce64e06103486f810266ea1052706ca3744
FedeAirala <fedeairala@gmail.com>
2024-07-22 17:52:42 -0300

    Ejercicios clase 2

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -6,21 +6,63 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Crea una variable para cada operación aritmética
 
+varSuma = (7+7+7)
+varResta = (15-10-3)
+varMult = (6*5)
+varDiv = (10/5)
+varMod = (10%2)
+varExp = (5**3)
+
+
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
 // 3. Imprime 5 comparaciones verdades con diferentes operadores de comparación
 
+console.log(0==0)
+console.log(1==true)
+console.log(0==false)
+console.log (false==false)
+console.log(true==true)
+
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+console.log(0==1)
+console.log(0==true)
+console.log(1==false)
+console.log (true==false)
+console.log(false==true)
 
 // 5. Utiliza el operador lógico and
 
+console.log (0==true && 1==false)
+console.log (0==false && 1==true)
+
 // 6. Utiliza el operador lógico or
+
+console.log (0==false || 1==true)
+console.log (0==false || 1==true)
+
 
 // 7. Combina ambos operadores lógicos
 
+console.log (0==true || (0==false && 1==true))
+
 // 8. Añade alguna negación
+let a = 6
+console.log (a== 5 || a==true)
+
+
 
 // 9. Utiliza el operador ternario
 
+const isRaining = true
+isRaining ? console.log("Está lloviendo") : console.log("No está lloviendo")
+
+const isEat = 0
+isEat ? console.log ("Está comiendo") : console.log ("No está comiendo")
+
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+
+console.log ((5+5)== 10 && (10-10)==false)
+console.log ((5+5)== 10 && (10-10)==true)
--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -6,20 +6,58 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Concatena dos cadenas de texto
 
+let text1 = "Fede "
+let text2 = "Airala "
+console.log (text1 + text2 + "!")
+
 // 2. Muestra la longitud de una cadena de texto
+
+let frase = "Hola Javascript"
+
+console.log(`La frase, ${frase} , tiene ${frase.length} espacios`)
+
 
 // 3. Muestra el primer y último carácter de un string
 
+console.log (`La frase ${frase} tiene como primer caracter a ${frase.slice(0,1)}`)
+console.log (`La frase ${frase} tiene como último caracter a ${frase.slice(-1)}`)
+
 // 4. Convierte a mayúsculas y minúsculas un string
+
+console.log (frase.toLowerCase())
+console.log (frase.toUpperCase())
 
 // 5. Crea una cadena de texto en varias líneas
 
+let cadena = `Cadena
+    de texto
+    en varias Líneas`
+console.log (cadena)
+
 // 6. Interpola el valor de una variable en un string
 
+let varValor = 44
+console.log (`Soy Fede y tengo ${varValor} años`)
+
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
+let texto = "Hola estoy practicando strings en Javascript"
+console.log (texto.replaceAll(" ","-"))
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
 
+console.log (texto.includes("practica")) // comprueba por segmento del string solicitado
+console.log (texto.includes("mundo")) 
+
 // 9. Comprueba si dos strings son iguales
 
+console.log ("hola" == "hola")
+console.log ("hola" == "Hola") // diferencia entre mayúsculas y minusculas
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+let texto1 = "Hola"
+let texto2 = "Fede"
+let texto3 = "Mundo"
+
+console.log (`Pimer comprobación es ${texto1.length==texto2.length}`)
+console.log (texto1.length==texto3.length)
